a6c82cbeff5f5d4116e76862d21f3eb45cc4ab06
Matheus <stadtloberm@gmail.com>
2026-03-27 09:43:46 -0300

    Updates 27/03

--- aula_09/README.md
+++ aula_09/README.md
@@ -24,3 +24,8 @@ not -> Ele altera o valor booleano da condição.
 ``if (se)`` -> verifica se uma condição é true (verdadeira). Se for, ele executa o código.
 ``elif (senão se)`` -> é usado para testar várias condições. Ele só executa se todas as condições anterires forem falsas.
 ``else (senão)`` -> executa o código se a condição if for false (falsa).
+
+## Boas Práticas
+1. Qualquer variável em python utiliza o padrão de case snake_case ou recentemente o cammelCase.
+2. Se você observar alguma estrutura tipo nome(), 90% de chance de ser uma função.
+3. Python não tem constante, porém utilizamos o padrão case UPPERCASE, para simular que aquela variáveç não pode ser alterada.
--- aula_10/estrutura_pizzaria_promocoes.py
+++ aula_10/estrutura_pizzaria_promocoes.py
@@ -7,8 +7,19 @@ dia_semana = input("Informe o dia da semana - [quarta], [quinta], [sexta], [sáb
 print(f"O sabor escolhido da pizza é {pizza_sabor}, o tamanho é {pizza_tamanho} e hoje é {dia_semana}.")
 
 # Promoções -> Estruturas Condicionais
+
 # Comprando qualquer pizza e qualquer tamanho no sábado, o refri é gratuito.
 
 # Comprando uma pizza de cabalebra tamanho médio, em qualquer dia, o frete é grátis.
 
 # Comprando qualquer pizza de qualquer tamanho no domingo, o frete e o refri são gratuitos.
+
+if dia_semana == "sábado":
+    print(f"🍕 Pedido aceito com sucesso!")
+    print(f"O refri é hoje é por conta da casa.")
+elif dia_semana == "domingo": 
+    print(f"🍕 Pedido aceito com sucesso!")
+    print(f"O refri e o frete hoje é por conta da casa.")
+elif pizza_sabor == "calabresa" and pizza_tamanho == "médio":
+    print(f"🍕 Pedido aceito com sucesso!")
+    print(f"O frete hoje é por conta da casa.")
